3285ffcc6ea179f4891d9057704d38727e725916
Russel Neiss <russel.neiss@gmail.com>
2017-05-21 21:29:23 -0500

    Audio Playing for right aliyah

--- index.ios.js
+++ index.ios.js
@@ -21,6 +21,7 @@ import {
 } from 'react-navigation';
 
 
+// Import Texts
 import Amos from './data/torah/json/Amos.json';
 import Deuteronomy from './data/torah/json/Deuteronomy.json';
 import Exodus from './data/torah/json/Exodus.json';
@@ -42,6 +43,22 @@ import Obadiah from './data/torah/json/Obadiah.json';
 import Samuel_1 from './data/torah/json/Samuel_1.json';
 import Samuel_2 from './data/torah/json/Samuel_2.json';
 import Zechariah from './data/torah/json/Zechariah.json';
+
+// Import Translations
+import GenesisTrans from './data/torah/translation/Genesis.json';
+import ExodusTrans from './data/torah/translation/Exodus.json';
+import DeuteronomyTrans from './data/torah/translation/Deuteronomy.json';
+import LeviticusTrans from './data/torah/translation/Leviticus.json';
+import NumbersTrans from './data/torah/translation/Numbers.json';
+
+
+
+// Import the react-native-sound module
+var Sound = require('react-native-sound');
+
+// Enable playback in silence mode (iOS only)
+Sound.setCategory('Playback');
+
 
 const loadTorahReadingScreen = () => {
   Alert.alert('Button has been pressed!');
@@ -130,7 +147,7 @@ class AliyahSelectScreen extends React.Component {
   render() {
     const { params } = this.props.navigation.state;
     const { navigate } = this.props.navigation;
-    var content = params.aliyot.map((obj) => (<CustomButton doOnPress={() => navigate('PlayViewScreen', { parshah: obj._id, aliyotStart: obj._begin, aliyotEnd: obj._end, length: obj._numverses, title: params.parshah, originatingBook: params.originatingBook })} buttonTitle={obj._num !="M" ? "Aliyah "+obj._num+": "+obj._begin+"-"+obj._end : "Maftir Aliyah"+": "+obj._begin+"-"+obj._end} />) );
+    var content = params.aliyot.map((obj) => (<CustomButton doOnPress={() => navigate('PlayViewScreen', { parshah: obj._id, aliyotStart: obj._begin, aliyotEnd: obj._end, length: obj._numverses, title: params.parshah, originatingBook: params.originatingBook, aliyahNum: obj._num })} buttonTitle={obj._num !="M" ? "Aliyah "+obj._num+": "+obj._begin+"-"+obj._end : "Maftir Aliyah"+": "+obj._begin+"-"+obj._end} />) );
 
     return (
       <View>
@@ -145,68 +162,164 @@ class PlayViewScreen extends React.Component {
   static navigationOptions = ({ navigation }) => ({
     title: `${navigation.state.params.title}`,
   });
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      audio: "",
+    };
+  }
+
+  componentDidMount() {
     const { params } = this.props.navigation.state;
-    var chapterStart = params.aliyotStart.split(':')[0];
-    var verseStart = params.aliyotStart.split(':')[1];
-    var chapterEnd = params.aliyotEnd.split(':')[0];
-    var verseEnd = params.aliyotEnd.split(':')[1];
-    var chapterStartIndex = chapterStart -1;
-    var verseStartIndex = verseStart -1;
-
-    var bookText = (function(book) {
-      switch(book) {
-        case 'Amos': return Amos.Tanach.tanach.book;
-        case 'Deuteronomy': return Deuteronomy.Tanach.tanach.book;
-        case 'Exodus': return Exodus.Tanach.tanach.book;
-        case 'Ezekiel': return Ezekiel.Tanach.tanach.book;
-        case 'Genesis': return Genesis.Tanach.tanach.book;
-        case 'Hosea': return Hosea.Tanach.tanach.book;
-        case 'Isaiah': return Isaiah.Tanach.tanach.book;
-        case 'Jeremiah': return Jeremiah.Tanach.tanach.book;
-        case 'Joel': return Joel.Tanach.tanach.book;
-        case 'Joshua': return Joshua.Tanach.tanach.book;
-        case 'Judges': return Judges.Tanach.tanach.book;
-        case 'Kings_1': return Kings_1.Tanach.tanach.book;
-        case 'Kings_2': return Kings_2.Tanach.tanach.book;
-        case 'Leviticus': return Leviticus.Tanach.tanach.book;
-        case 'Malachi': return Malachi.Tanach.tanach.book;
-        case 'Micah': return Micah.Tanach.tanach.book;
-        case 'Numbers': return Numbers.Tanach.tanach.book;
-        case 'Obadiah': return Obadiah.Tanach.tanach.book;
-        case 'Samuel_1': return Samuel_1.Tanach.tanach.book;
-        case 'Samuel_2': return Samuel_2.Tanach.tanach.book;
-        case 'Zechariah': return Zechariah.Tanach.tanach.book;
+    var audioFileName = "audio/"+params.title.replace(/[ ’]/g, '')+"-"+ params.aliyahNum+".mp3";
+    var aliyahAudio = new Sound(audioFileName, Sound.MAIN_BUNDLE, (error) => {
+      if (error) {
+        console.log('failed to load the sound', error);
+        return;
       }
+      this.setState({audio: aliyahAudio});
     });
+  }
 
-    var verseWords = (function(verse) {
-      var words = "";
-      for (i = 0; i < verse.w.length; i++) {
-          words += verse.w[i] + " ";
-      }
-      return words;
-    });
+  componentWillUnmount() {
+      this.state.audio.release();
+  }
+  render() {
 
-
-    var textFile = (function(book, sectionLength) {
-      var selectedBook = bookText(book);
-      var firstVerse = selectedBook.c[chapterStartIndex].v[verseStartIndex];
-
-      var textToReturn = verseWords(firstVerse);
-
-      return textToReturn;
-
-    });
-
-
+    if (this.state.audio == "") {
     return (
       <View>
-        <Text>{textFile(params.originatingBook, params.length)}</Text>
-        <Text>{chapterStart}:{verseStart}</Text>
-        <Text>{chapterEnd}:{verseEnd}</Text>
+        <Text>Loading....</Text>
       </View>
     );
+
+    }
+
+    else {
+      const {params} = this.props.navigation.state;
+      var chapterStart = params.aliyotStart.split(':')[0];
+      var verseStart = params.aliyotStart.split(':')[1];
+      var chapterEnd = params.aliyotEnd.split(':')[0];
+      var verseEnd = params.aliyotEnd.split(':')[1];
+      var chapterStartIndex = chapterStart - 1;
+      var verseStartIndex = verseStart - 1;
+      var curWordIndex = 0;
+
+      var bookText = (function(book) {
+        switch (book) {
+          case 'Amos':
+            return Amos.Tanach.tanach.book;
+          case 'Deuteronomy':
+            return Deuteronomy.Tanach.tanach.book;
+          case 'Exodus':
+            return Exodus.Tanach.tanach.book;
+          case 'Ezekiel':
+            return Ezekiel.Tanach.tanach.book;
+          case 'Genesis':
+            return Genesis.Tanach.tanach.book;
+          case 'GenesisTrans':
+            return GenesisTrans;
+          case 'ExodusTrans':
+            return ExodusTrans;
+          case 'DeuteronomyTrans':
+            return DeuteronomyTrans;
+          case 'LeviticusTrans':
+            return LeviticusTrans;
+          case 'NumbersTrans':
+            return NumbersTrans;
+          case 'Hosea':
+            return Hosea.Tanach.tanach.book;
+          case 'Isaiah':
+            return Isaiah.Tanach.tanach.book;
+          case 'Jeremiah':
+            return Jeremiah.Tanach.tanach.book;
+          case 'Joel':
+            return Joel.Tanach.tanach.book;
+          case 'Joshua':
+            return Joshua.Tanach.tanach.book;
+          case 'Judges':
+            return Judges.Tanach.tanach.book;
+          case 'Kings_1':
+            return Kings_1.Tanach.tanach.book;
+          case 'Kings_2':
+            return Kings_2.Tanach.tanach.book;
+          case 'Leviticus':
+            return Leviticus.Tanach.tanach.book;
+          case 'Malachi':
+            return Malachi.Tanach.tanach.book;
+          case 'Micah':
+            return Micah.Tanach.tanach.book;
+          case 'Numbers':
+            return Numbers.Tanach.tanach.book;
+          case 'Obadiah':
+            return Obadiah.Tanach.tanach.book;
+          case 'Samuel_1':
+            return Samuel_1.Tanach.tanach.book;
+          case 'Samuel_2':
+            return Samuel_2.Tanach.tanach.book;
+          case 'Zechariah':
+            return Zechariah.Tanach.tanach.book;
+        }
+      });
+
+      if (this.state.audio) {
+        this.state.audio.play();
+      }
+
+
+      var verseWords = (function(verse) {
+        var words = [];
+        for (i = 0; i < verse.w.length; i++) {
+          words.push(<TouchableOpacity key={i}><Text style={styles.word}>
+            {verse.w[i].replace(/\//g, '')}
+          </Text></TouchableOpacity>);
+          curWordIndex++;
+        }
+        return words;
+      });
+
+      var verses = (function(book, transBook, chapterStart, verseStart, length) {
+        var verseText = [];
+        var curChapter = chapterStart;
+        var curVerse = verseStart;
+        for (q = 0; q < length; q++) {
+          if (!book.c[curChapter].v[curVerse]) {
+            curChapter = curChapter + 1;
+            curVerse = 0;
+          }
+          verseText.push(<View style={styles.text}>
+            <Text>{curChapter + 1}:{curVerse + 1}</Text>{verseWords(book.c[curChapter].v[curVerse])}
+            <Text>{transBook.text[curChapter][curVerse]}</Text>
+          </View>);
+          curVerse = curVerse + 1;
+        }
+
+        return verseText;
+
+      });
+
+
+      var textFile = (function(book, sectionLength) {
+
+        var selectedBook = bookText(book);
+        var selectedTrans = bookText(book + "Trans");
+//      var firstVerse = selectedBook.c[chapterStartIndex].v[verseStartIndex];
+
+        var textToReturn = verses(selectedBook, selectedTrans, chapterStartIndex, verseStartIndex, sectionLength);
+
+        return textToReturn;
+
+      });
+
+
+      return (
+        <View>
+          <ScrollView>
+            <View style={styles.text}>{textFile(params.originatingBook, params.length)}</View>
+          </ScrollView>
+        </View>
+      );
+    }
   }
 }
 
@@ -247,7 +360,21 @@ const styles = StyleSheet.create({
     padding: 10,
     backgroundColor: '#ccc',
     textAlign: 'center',
+  },
+
+  text: {
+    flexDirection:'row-reverse',
+    flexWrap:'wrap',
+    alignItems:'flex-start',
+  },
+
+  word: {
+    flex:0,
+    padding: 4,
+    fontSize: 36,
+    fontFamily: "Taamey Frank Taamim Fix",
   }
+
 });
 
 AppRegistry.registerComponent('PocketTorah', () => PocketTorah);
